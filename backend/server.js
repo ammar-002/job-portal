50a0123b8d59e0e164184437c811a108fbee4edf
Muhammad Ammar <iammuhammadammar2@gmail.com>
2026-07-30 02:00:42 +0500

    fix: add CORS preflight support for all routes

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
 }))
+app.options("*", cors())
 app.use(express.json())  //parse json data into js data as express doesnt know how to handle json data
 app.use(cookieParser())  
 
